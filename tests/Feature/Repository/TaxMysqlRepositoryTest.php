3d8eeee31f7696ca0f1b67db423fa00f06dec5b3
behnam <behnamtakta71@gmail.com>
2022-08-25 00:06:58 +0430

    create OverallAmountOfTaxesPerState test

--- tests/Feature/Repository/TaxMysqlRepositoryTest.php
+++ tests/Feature/Repository/TaxMysqlRepositoryTest.php
@@ -2,19 +2,56 @@
 
 namespace Tests\Feature\Repository;
 
+use App\Models\County;
+use App\Models\CountyIncome;
+use App\Models\State;
+use App\Repository\Tax\TaxMysqlRepository;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TaxMysqlRepositoryTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+
+
     public function testOverallAmountOfTaxesPerState()
     {
+        //arrange
+        $count1TaxRate=0.1;
+        $count2TaxRate=0.2;
+        $count1Amount=80000;
+        $count2Amount=690000;
+        $state=State::factory()
+            ->has(County::factory()
+                ->state([
+                    'tax_rate'=>$count1TaxRate
+                ])
+                ->has(CountyIncome::factory()
+                    ->state([
+                        'amount'=>$count1Amount
+                    ])
+                )
+            )
+            ->has(County::factory()
+                ->state([
+                    'tax_rate'=>$count2TaxRate
+                ])
+                ->has(CountyIncome::factory()
+                    ->state([
+                        'amount'=>$count2Amount
+                    ])
+                )
+            )
+            ->create();
 
+        $expected=($count1Amount * $count1TaxRate) + ($count2Amount * $count2TaxRate);
+
+        //act
+        $TaxMysqlRepository=new TaxMysqlRepository();
+        $result=$TaxMysqlRepository->overallAmountOfTaxesPerState($state->id);
+
+        //assert
+
+        $this->assertEquals($expected,$result);
     }
 }
